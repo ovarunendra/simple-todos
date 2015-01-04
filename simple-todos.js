a9579f52a4958d5f88dfd6e5f3a90f55a3234d4a
Varunendra Pratap Singh <ovarunendra@zeomega.com>
2015-01-04 23:40:51 +0530

    Storing tasks in a collection

--- simple-todos.js
+++ simple-todos.js
@@ -1,11 +1,11 @@
-if (Meteor.isClient) {
+Tasks = new Mongo.Collection("tasks");
 
+if (Meteor.isClient) {
+  // This code only runs on the client
   Template.body.helpers({
-    tasks: [
-      {text: "This is task 1"},
-      {text: "This is task 2"},
-      {text: "This is task 3"}
-    ]
+    tasks: function () {
+      return Tasks.find({});
+    }
   });
 }
 
