230ada79951f139f88566d48b41f6e0d7bb24f57
Varunendra Pratap Singh <ovarunendra@zeomega.com>
2015-01-04 23:31:33 +0530

    worked with templates

--- simple-todos.js
+++ simple-todos.js
@@ -1,18 +1,11 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
+  Template.body.helpers({
+    tasks: [
+      {text: "This is task 1"},
+      {text: "This is task 2"},
+      {text: "This is task 3"}
+    ]
   });
 }
 
